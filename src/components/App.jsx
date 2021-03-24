75774731fcf78c9cc470bed29ad71de3cdf38e83
Sayanti Sarkar <68384931+sayantisarkar@users.noreply.github.com>
2021-03-24 10:42:00 +0400

    Added ternaru and && operator instead of function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,26 @@ import Form from "./Form";
 
 var userIsRegistered = true;
 
-function checkUser() {
-  if (userIsRegistered) {
-    return <Form name="Login" />;
-  } else {
-    return <Form name="Register" />;
-  }
-}
+// function checkUser() {
+//   if (userIsRegistered) {
+//     return <Form name="Login" />;
+//   } else {
+//     return <Form name="Register" />;
+//   }
+// }
 
 function App() {
-  return <div className="container">{checkUser()}</div>;
+  //return <div className="container">{checkUser()}</div>;
+  return (
+    <div className="container">
+      {/* Ternary */}
+      {/* {userIsRegistered ? <Form name="Login" /> : <Form name="Register" />} */}
+
+      {/* && Operator */}
+      {userIsRegistered && <Form name="Login" />}
+      {!userIsRegistered && <Form name="Register" />}
+    </div>
+  );
 }
 
 export default App;
